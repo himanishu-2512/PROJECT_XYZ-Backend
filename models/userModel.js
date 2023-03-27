d090fe15441735470584b8a89b29965d4e15d801
Asif <rahmanasif158@gmail.com>
2023-03-27 22:40:10 +0530

    Created userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,52 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-    rNum: String,
-    dob: String,
-    pass: String
+    username: { 
+        type: String, 
+        unique: true, 
+        require: true 
+    },
+
+    email: { 
+        type: String, 
+        unique: true, 
+        require: true },
+
+    name: String,
+
+    password: { 
+        type: String, 
+        unique: true, 
+        require: true, 
+        min: [8, 'Must be at least 8 character, got {VALUE}'], },
+
+    college: [{ 
+        type: String 
+    }],
+
+    city: String,
+
+    bio: String,
+
+    dob: Date,
+
+    skills: [{ 
+        type: String 
+    }],
+
+    isVerified: Boolean,
+
+    isAdmin: Boolean,
+
+    following: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: "User" 
+    }],
+
+    followerCount: [{ 
+        type: Number, 
+        default: 0 
+    }]
 })
 
 
-module.exports=mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema)
