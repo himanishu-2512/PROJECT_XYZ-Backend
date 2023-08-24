7dcecf9efb91142c867b81b1a1e4d24769f4fd07
Himanshu Singh <himanishu93@gmail.com>
2023-08-24 12:22:20 +0530

    Feat: new routes to save and view savaed posts and questions

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
-const {createQuestion, updateQuestion,deleteQuestion, allQuestions, newAnswer, updateAnswer, deleteAnswer} = require("../controllers/questionController")
+const {createQuestion,
+     updateQuestion,
+     deleteQuestion,
+      allQuestions,
+       newAnswer,
+        updateAnswer,
+         deleteAnswer,
+         userquestions,
+         savequestions,
+         getsavequestions,
+         getquestionbyid} = require("../controllers/questionController")
 
 
 router.post("/newquestion", createQuestion);
@@ -7,6 +17,10 @@ router.post("/updatequestion/:userId/:questionId", updateQuestion);
 router.delete("/deletequestion/:userId/:questionId", deleteQuestion);
 router.get("/allquestions", allQuestions);
 router.post("/answer/newanswer/:userId/:questionId", newAnswer);
+router.get("/questions/user/:username",userquestions);
+router.post("/questions/savequestions/:questionId",savequestions)
+router.get("/getsavequestions/:userId",getsavequestions)
+router.get("/getquestion/:questionId",getquestionbyid)
 router.post("/answer/updateanswer/:userId/:questionId/:answerId", updateAnswer)
 router.post("/answer/deleteanswer/:userId/:questionId/:answerId", deleteAnswer)
 
